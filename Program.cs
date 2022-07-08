be50d954d243eb40109dfe69307d9c19ab45a3b2
fedesito <49609909+lbertitoyt@users.noreply.github.com>
2022-07-08 16:36:20 +0200

    fixed shit

--- Program.cs
+++ Program.cs
@@ -11,21 +11,22 @@ string[] foundStuff = exeFiles.ToArray();
 
 try { 
     for (int i = 0; i < foundStuff.Length; i++) {
-    File.Move(foundStuff[i], foundStuff[i].Remove(foundStuff[i].Length - 7) + "1.prefab");
+    File.Move(foundStuff[i], foundStuff[i].Remove(foundStuff[i].Length - 7) + "fa.prefab");
 } } catch {
     Console.WriteLine("unable to rename files, they're being used by a process already");
+    Console.ReadLine();
 }
 } else if (option.KeyChar == '2') {
     var exeFiles1 = Directory.EnumerateFiles("./", 
                                         "*", SearchOption.AllDirectories)
-               .Where(s => s.EndsWith("1.prefab"))
+               .Where(s => s.EndsWith("fa.prefab"))
                .ToList();
                
 string[] foundStuff1 = exeFiles1.ToArray();
 
 try { 
     for (int i = 0; i < foundStuff1.Length; i++) {
-    File.Move(foundStuff1[i], foundStuff1[i].Remove(foundStuff1[i].Length - 8) + ".prefab");
+    File.Move(foundStuff1[i], foundStuff1[i].Remove(foundStuff1[i].Length - 9) + ".prefab");
 } } catch {
     Console.WriteLine("unable to rename files, they're being used by a process already");
 }}
